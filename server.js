84cc35117d6e7c11e8daae6e1839af9dd890a40f
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2022-08-20 18:14:41 -0500

    ahhhhhhhhhhhhh

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser')
 const indexRouter = require("./routes/index")
 
 if(process.env.NODE_ENV === "production") {
-  express.static("client/build")
+  app.use(express.static("client/build"))
 
   const path = require("path");
   app.get("*", (req, res) => {
