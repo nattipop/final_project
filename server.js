66f59422021f2b54cba9b3d1bfce1342f27c1aeb
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2022-08-24 19:52:11 -0500

    server edits

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json())
 
 const mongoose = require("mongoose");
 console.log(keys.MONGODB_URI)
-mongoose.connect(keys.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(keys.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const db = mongoose.connection;
 db.on("error", error => console.log(error))
 db.once("open", () => console.log("connected to database"))
