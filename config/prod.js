2ae27f40a0667b62d995adbaef0dfa753715610a
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-01-13 17:07:27 -0600

    url again

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,7 @@ module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
   TOKEN_SECRET: process.env.TOKEN_SECRET,
   API_KEY: process.env.API_KEY,
-  url:  "https://final-project-parsity.herokuapp.com"
+  url:  "https://final-project-parsity.herokuapp.com",
+  EMAIL_USERNAME: "nattipop815@gmail.com",
+  EMAIL_PASS: "kawvgjmvejjohqzm"
 };
