f08129ba87700724ca7b2710480d21caf2b8a21b
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-01-16 16:00:50 -0600

    test route

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,9 @@ function tokenForUser(user) {
     exp: Math.round(Date.now() / 1000 + 5 * 60 * 60)}, keys.TOKEN_SECRET)
 };
 
+router.get("/verify-user-email", (req, res) => {
+  res.send("hi")
+})
 router.post('/create-checkout-session', async (req, res) => {
   const { price } = req.body.price;
 
