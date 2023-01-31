60a1a066db42d25b5414cafc6e4165f26d06babd
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-01-30 22:26:06 -0600

    build public

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ db.once("open", () => console.log("connected to database"))
 
 app.use("/", indexRouter);
 app.use(express.static('client/build'));
-app.get('*', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.resolve('client', 'public', 'index.html')));
 
 app.listen(process.env.PORT || 3000)
