5a83974de99aa5ab2ea5c08baca939b8d72202e3
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-03-09 14:50:21 -0600

    trying another angle

--- server.js
+++ server.js
@@ -24,10 +24,29 @@ app.use(express.static("public"));
 app.use(bodyParser.json())
 
 const mongoose = require("mongoose");
+const { default: Menu } = require("./client/src/components/Menu");
 mongoose.connect(keys.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", error => console.log(error))
 db.once("open", () => console.log("connected to database"))
+
+app.get('/', (req, res) => {
+  const app = ReactDOMServer.renderToString(<Menu />);
+  const indexFile = path.resolve('./build/index.html');
+
+  fs.readFile(indexFile, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Something went wrong:', err);
+      return res.status(500).send('Oops, better luck next time!');
+    }
+
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+    );
+  });
+});
+
+app.use(express.static('./build'));
 
 app.use("/api", indexRouter);
 
